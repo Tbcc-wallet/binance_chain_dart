f23955e46793f33f33c32a1c789868ada9d1dc46
Oleg Ryzhov <robbbbb748@gmail.com>
2021-09-07 17:46:15 +0300

    fix pointycastle

--- lib/src/utils/crypto.dart
+++ lib/src/utils/crypto.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'package:collection/collection.dart' show IterableExtension;
 import 'package:convert/convert.dart';
+import 'package:pointycastle/digests/keccak.dart';
 import 'package:pointycastle/digests/ripemd160.dart';
 import 'package:pointycastle/digests/sha256.dart';
 import 'package:pointycastle/api.dart' show KeyParameter;
-import 'package:pointycastle/digests/sha3.dart';
 import 'package:pointycastle/macs/hmac.dart';
 import 'package:pointycastle/digests/sha512.dart';
 
@@ -232,7 +232,7 @@ Uint8List publicKeyToAddress(Uint8List publicKey) {
 const int _shaBytes = 256 ~/ 8;
 // keccak is implemented as sha3 digest in pointycastle, see
 // https://github.com/PointyCastle/pointycastle/issues/128
-final SHA3Digest sha3digest = SHA3Digest(_shaBytes * 8);
+final KeccakDigest sha3digest = KeccakDigest(_shaBytes * 8);
 
 Uint8List keccak256(Uint8List input) {
   sha3digest.reset();
